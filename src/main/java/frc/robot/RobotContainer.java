96bc491394f3b21a56ae263728514ea6816feedd
Kristian <kristiandehaan@gmail.com>
2024-12-18 09:02:30 -0600

    Adding function to home wheels

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -59,8 +59,8 @@ public class RobotContainer {
      */
     private void configureButtonBindings() {
         /* Driver Buttons */
-        zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroHeading()));
-        setWheelsToZero.onTrue(new InstantCommand(() -> s_Swerve.resetModulesToAbsolute()));
+        zeroGyro.onTrue(new InstantCommand(s_Swerve::zeroHeading));
+        setWheelsToZero.onTrue(new InstantCommand(s_Swerve::alignStraight));
     }
 
     /**
