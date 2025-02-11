08b7590875c3adbcbaa9286dea1093b8ea927c60
Rico6232 <rggarcia1310@gmail.com>
2025-02-11 17:49:24 -0600

    Working on elevator and added Rev dep

---
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.*;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
