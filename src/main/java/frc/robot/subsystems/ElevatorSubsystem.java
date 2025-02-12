b78dbba02e7096e97f9f50c2ee8c2acb49c44899
Rico6232 <rggarcia1310@gmail.com>
2025-02-11 18:15:41 -0600

    working

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,7 +1,28 @@
 package frc.robot.subsystems;
 
+
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import com.revrobotics.*;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+
+public  class ElevatorSubsystem implements Subsystem
+{
+    private final SparkMax elevatorMotor;
+    private final SparkMax wristMotor;
+    private final DigitalInput elevatorSwitch;
+    private final DigitalInput wristSwitch;
+
+
+    public ElevatorSubsystem()
+    {
+        // TODO: Set the default command, if any, for this subsystem by calling setDefaultCommand(command)
+        //       in the constructor or in the robot coordination class, such as RobotContainer.
+
+
+
+    }
+}
+
