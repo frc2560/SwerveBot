718c17073111afe38e81c1a0001038dd38ded023
Richard G. <rggarcia1310@gmail.com>
2025-02-19 21:44:29 -0600

    Left Coral Scoring...?
    doing my work so kristian plays fortnite with me pt3

---
+++ src/main/java/frc/robot/commands/grabCoral/Left/GrabCoralL1CommandLeft.java
@@ -0,0 +1,42 @@
+package frc.robot.commands.grabCoral.Left;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
+import frc.robot.commands.coral.CoralOutTakeCommand;
+import frc.robot.commands.coral.SetCoralArmL1L2L3Command;
+import frc.robot.commands.elevator.GoToBottomCommand;
+import frc.robot.commands.elevator.GoToL1Command;
+import frc.robot.commands.movement.AlignWithTag;
+import frc.robot.subsystems.CoralSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
+import frc.robot.subsystems.Swerve;
+
+
+public class GrabCoralL1CommandLeft extends SequentialCommandGroup {
+    public GrabCoralL1CommandLeft(CoralSubsystem coralSubsystem, ElevatorSubsystem elevatorSubsystem, Swerve swervesubsystem) {
+        super(
+                new AlignWithTag(
+                        swervesubsystem,
+                        Constants.AlignToTag.LeftReef_AREA_STAGE,
+                        Constants.AlignToTag.LeftReef_Y,
+                        Constants.AlignToTag.LeftReef_OMEGA),
+                new SetCoralArmL1L2L3Command(coralSubsystem),
+                new GoToL1Command(elevatorSubsystem),
+                new AlignWithTag(
+                        swervesubsystem,
+                        Constants.AlignToTag.LeftReef_AREA_STAGE,
+                        Constants.AlignToTag.LeftReef_Y,
+                        Constants.AlignToTag.LeftReef_OMEGA),
+                new CoralOutTakeCommand(coralSubsystem),
+                new AlignWithTag(
+                        swervesubsystem,
+                        Constants.AlignToTag.LeftReef_AREA_STAGE,
+                        Constants.AlignToTag.LeftReef_Y,
+                        Constants.AlignToTag.LeftReef_OMEGA),
+                new GoToBottomCommand(elevatorSubsystem)
+        );
+    }
+}
+
+
+
