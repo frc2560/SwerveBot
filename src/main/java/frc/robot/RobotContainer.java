bd5d5bc13642459c2aefd4caa1d26c2bb8df7d3d
Tonanci <s149019@grandviewc4.net>
2025-03-21 17:39:43 -0500

    Tuning from day two

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -90,7 +90,7 @@ public class RobotContainer {
                     )
             );
 
-        // Configure the button bindings
+        // Configure the button binding
         configureButtonBindings();
     }
 
